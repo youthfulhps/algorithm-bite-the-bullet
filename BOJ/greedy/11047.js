80b98d289055950d27e5d7954cefb5a35db59f98
youthfulhps <ybh942002@gmail.com>
2023-04-07 23:44:29 +0900

    Solve: 11047

--- BOJ/greedy/11047.js
+++ BOJ/greedy/11047.js
@@ -1,5 +1,21 @@
-const inputs = require("fs").readFileSync("dev/stdin").toString().split("\n");
+const inputs = require("fs").readFileSync("./input.txt").toString().trim().split("\n");
 
-function solution(inputs) {}
+function solution(inputs) {
+  let [_, K] = inputs.shift().split(" ").map(Number);
+  const coins = inputs.map(Number).filter(input => input <= K).reverse();
+
+  let result = 0;
+
+  for (let coin of coins) {
+    if (K === 0) {
+      break;
+    }
+
+    result += Math.floor(K / coin);
+    K %= coin;
+  }
+
+  return result;
+}
 
 console.log(solution(inputs));
