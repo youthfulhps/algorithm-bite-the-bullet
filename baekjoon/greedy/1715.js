67ba9e0f3d83fe4bbb3d8eb11c50b72b27dd2030
youthfulhps <ybh942002@gmail.com>
2023-04-27 10:05:10 +0900

    Solve: 1715

--- baekjoon/greedy/1715.js
+++ baekjoon/greedy/1715.js
@@ -4,13 +4,8 @@
 // 합산 결과에 더한다.
 // 다시 오름차순으로 정렬한다. 반복!
 // 합산 후 다시 뽑을때도 가장 작은 두 뭉치를 뽑아야 한다. (최초 입력을 소팅한다고 해서 되지않음 e.g. 3 4 5 6)
-
-// 첫 시도는 입력값을 오름차순으로 정렬하고 하나씩 뭉치를 합쳐가면 된다고 생각했는데 다음과 같은 반례가 있음
-// 3 4 5 6
-// 3 + 4 이후 7 5 6 이 되었다면, 5 + 6 을 다음으로 합치는 것이 횟수가 적음
-// 합산 이후 그때 그때 적은 두 뭉치 찾아야 함
-
 // Fail: 메모리 초과 -> 배열말고 minHeap?
+// N === 1 일때는 비교가 필요없음, 즉 0
 
 const inputs = require("fs").readFileSync("./input.txt").toString().trim().split('\n').map(Number);
 
@@ -19,19 +14,80 @@ function solution(inputs) {
   const N = inputs.shift();
 
   if (N === 1) {
-    return inputs[0];
+    return 0;
   }
 
+  const minHeap = new MinHeap();
 
-  while(inputs.length > 1) {
-    inputs.sort((a, b) => a - b);
-    const partsSum = inputs.shift() + inputs.shift();
+  inputs.forEach(input => minHeap.push(input));
+
+  while(minHeap.heap.length > 2) {
+    const partsSum = minHeap.pop() + minHeap.pop();
 
     result += partsSum;
-    inputs.unshift(partsSum);
+    minHeap.push(partsSum);
   }
 
   return result;
 }
+
+class MinHeap {
+  constructor() {
+    this.heap = [ null ];
+  }
+
+  size() {
+    return this.heap.length - 1;
+  }
+
+  getMin() {
+    return this.heap[1] ? this.heap[1] : null;
+  }
+
+  swap(a, b) {
+    [ this.heap[a], this.heap[b] ] = [ this.heap[b], this.heap[a] ];
+  }
+
+  push(value) {
+    this.heap.push(value);
+    let curIdx = this.heap.length - 1;
+    let parIdx = (curIdx / 2) >> 0;
+
+    while(curIdx > 1 && this.heap[parIdx] > this.heap[curIdx]) {
+      this.swap(parIdx, curIdx)
+      curIdx = parIdx;
+      parIdx = (curIdx / 2) >> 0;
+    }
+  }
+
+  pop() {
+    const min = this.heap[1];
+    if(this.heap.length <= 2) this.heap = [ null ];
+    else this.heap[1] = this.heap.pop();
+
+    let curIdx = 1;
+    let leftIdx = curIdx * 2;
+    let rightIdx = curIdx * 2 + 1;
+
+    if(!this.heap[leftIdx]) return min;
+    if(!this.heap[rightIdx]) {
+      if(this.heap[leftIdx] < this.heap[curIdx]) {
+        this.swap(leftIdx, curIdx);
+      }
+      return min;
+    }
+
+    while(this.heap[leftIdx] < this.heap[curIdx] || this.heap[rightIdx] < this.heap[curIdx]) {
+      const minIdx = this.heap[leftIdx] > this.heap[rightIdx] ? rightIdx : leftIdx;
+      this.swap(minIdx, curIdx);
+      curIdx = minIdx;
+      leftIdx = curIdx * 2;
+      rightIdx = curIdx * 2 + 1;
+    }
+
+    return min;
+  }
+}
+
 
 console.log(solution(inputs));
