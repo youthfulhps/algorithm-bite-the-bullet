8e3804945cf81cc4ca702bbd4fc45b4b3adcc2ec
youthfulhps <ybh942002@gmail.com>
2023-05-06 23:26:28 +0900

    Solve: 2212

--- baekjoon/greedy/2212.js
+++ baekjoon/greedy/2212.js
@@ -1,26 +1,30 @@
-const inputs = require("fs").readFileSync("./input.txt").toString().trim().split('\n');
+// https://www.acmicpc.net/problem/13164 참고!
+
+const inputs = require("fs")
+  .readFileSync("./input.txt")
+  .toString()
+  .trim()
+  .split('\n');
 
 function solution(inputs) {
   const N = inputs.shift();
   const centerCount = Number(inputs.shift());
-  let result;
 
-  const set = new Set();
-
-  inputs
-    .shift()
+  const sensor = inputs.shift()
     .split(' ')
-    .forEach(input => set.add(Number(input)));
+    .map(Number)
+    .sort((a, b) => a - b);
 
-  const sensors = Array.from(set).sort((a,b) => a - b);
+  const gaps = [];
 
-  // 센서별로 거리를 구한다.
-  // 가장 좁게 붙어있는 곳에 먼저 기지를 세운다.
-  // 세울 수 있는 기지국 수만큼 반복
+  for (let i =0;i<N -1; i++) {
+    gaps.push(sensor[i+1] - sensor[i]);
+  }
 
-  console.log(sensors);
-
-  return result;
+  return gaps
+    .sort((a, b) => b - a)
+    .slice(centerCount -1)
+    .reduce((sum, curr) => sum + curr, 0);
 }
 
 console.log(solution(inputs));
