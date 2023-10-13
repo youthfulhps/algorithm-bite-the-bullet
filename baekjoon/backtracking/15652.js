1820985623d3602c44e8184f3733c3ae7e9167e6
youthfulhps <ybh942002@gmail.com>
2023-10-13 17:24:21 +0900

    Solve: 15652

---
+++ baekjoon/backtracking/15652.js
@@ -0,0 +1,34 @@
+const inputs = require("fs")
+  .readFileSync("./input.txt")
+  .toString()
+  .trim()
+  .split(' ')
+  .map(Number);
+
+function solution(inputs) {
+  const [N, M] = inputs;
+  const answer = [];
+
+  function back(used, idx) {
+    if (idx === M) {
+      answer.push(used.join(' '));
+      return;
+    }
+
+    for (let i = 1; i <= N; i++) {
+      if (used.length) {
+        if (i >= used[used.length - 1]) {
+          back([...used, i], idx + 1);
+        }
+      } else {
+        back([i], idx + 1);
+      }
+    }
+  }
+
+  back([], 0);
+
+  return answer.join('\n');
+}
+
+console.log(solution(inputs));
