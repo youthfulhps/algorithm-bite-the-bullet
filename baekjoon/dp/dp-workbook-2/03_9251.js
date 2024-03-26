bc8eacf79044c1df53e4f110907a9902e765eab6
youthfulhps <ybh942002@gmail.com>
2024-03-27 00:29:38 +0900

    Solve: 9251

--- baekjoon/dp/dp-workbook-2/03_9251.js
+++ baekjoon/dp/dp-workbook-2/03_9251.js
@@ -10,17 +10,12 @@ function solution(inputs) {
   const lenB = B.length;
   const memo = Array.from({length: lenA + 1}, () => Array(lenB + 1).fill(0));
 
-  for (let i = 0; i <= lenA; i++) {
-    for (let j = 0; j <= lenB; j++) {
-      if (i === 0 || j === 0) {
-        memo[i][j] = 0;
-        continue;
-      }
-
+  for (let i = 1; i <= lenA; i++) {
+    for (let j = 1; j <= lenB; j++) {
       if (A[i - 1] === B[j - 1]) {
         memo[i][j] = memo[i - 1][j - 1] + 1;
       } else {
-        memo[i][j] = Math.max(memo[i - 1][j], memo[i][j - 1]);
+        memo[i][j] = Math.max(memo[i][j - 1], memo[i - 1][j]);
       }
     }
   }
